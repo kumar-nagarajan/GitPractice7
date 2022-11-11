fc7cce3028bea2522fdf425297e07bbb4223fb8d
kumar nagarajan <kumar.nagarajan@tekschool.us>
2022-11-10 20:40:35 -0800

    third changes

--- src/GitPractice7/Test1.java
+++ src/GitPractice7/Test1.java
@@ -1,10 +1,14 @@
 package GitPractice7;
 
 public class Test1 {
-
+public void NewTest() {
+System.out.println("testing");	
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("Hi my testing git");
+Test1 test = new Test1();
+test.NewTest();
 	}
 
 }
