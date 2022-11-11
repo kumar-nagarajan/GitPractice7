dd396ee63b2ce45d914cbbeea94075d138c52401
kumar nagarajan <kumar.nagarajan@tekschool.us>
2022-11-10 20:47:25 -0800

    forth commit

--- src/GitPractice7/Test1.java
+++ src/GitPractice7/Test1.java
@@ -4,11 +4,18 @@ public class Test1 {
 public void NewTest() {
 System.out.println("testing");	
 }
+public void NewTest1() {
+System.out.println("this is new method");	
+}
+public void NewTest2() {
+System.out.println("this is new for remote");	
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hi my testing git");
 Test1 test = new Test1();
 test.NewTest();
+test.NewTest1();
 	}
 
 }
